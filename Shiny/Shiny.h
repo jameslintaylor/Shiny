4066ead1d76308facf5508be43825c17b092da65
JLT <jameslintaylor@gmail.com>
2016-02-12 22:17:05 -0500

    Initial Commit

---
+++ Shiny/Shiny.h
@@ -0,0 +1,19 @@
+//
+//  Shiny.h
+//  Shiny
+//
+//  Created by James Taylor on 2016-02-12.
+//  Copyright © 2016 James Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shiny.
+FOUNDATION_EXPORT double ShinyVersionNumber;
+
+//! Project version string for Shiny.
+FOUNDATION_EXPORT const unsigned char ShinyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shiny/PublicHeader.h>
+
+
