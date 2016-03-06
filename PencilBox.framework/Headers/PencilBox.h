0c794e096aefe4f6464acabd2a608c8f5f25153a
James Taylor <jameslintaylor@gmail.com>
2016-03-06 01:21:30 -0500

    Particle System implementation

---
+++ PencilBox.framework/Headers/PencilBox.h
@@ -0,0 +1,19 @@
+//
+//  PencilBox.h
+//  PencilBox
+//
+//  Created by James Taylor on 2016-02-12.
+//  Copyright © 2016 James Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PencilBox.
+FOUNDATION_EXPORT double PencilBoxVersionNumber;
+
+//! Project version string for PencilBox.
+FOUNDATION_EXPORT const unsigned char PencilBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PencilBox/PublicHeader.h>
+
+
